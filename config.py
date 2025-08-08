94d18f42df264adf1f416e9b6a537083ea40efbd
nehraraman <157287892+nehraraman@users.noreply.github.com>
2025-08-08 17:24:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# Telegram Bot Token
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8335379785:AAHK01gK2gaAsF75DoAD0tf-vNPKyJeUq_I")
+
+# Admin Telegram ID
+ADMIN_ID = int(os.getenv("ADMIN_ID", 1756808131))
